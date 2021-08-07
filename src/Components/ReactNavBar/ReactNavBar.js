6fca02212cd4cbdba1ce0807bdd2fadf5167fdfb
Mohsin Ali <55811423+mosin92@users.noreply.github.com>
2021-08-07 12:23:26 +0500

    update

---
+++ src/Components/ReactNavBar/ReactNavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {wrapper} from './navbar.module.css'
+function ReactNavBar() {
+    return (
+        
+        <div classname={wrapper}>
+            React Nav Bar
+        </div>
+    )
+}
+
+export default ReactNavBar
